72492a36083a7c36829ba8766dba3a9cf338d379
Thanh Pham Van <thanhpv@sandinh.net>
2017-09-13 15:54:32 +0700

    change the Homepage

--- front/src/components/home/home.ts
+++ front/src/components/home/home.ts
@@ -3,13 +3,53 @@ import Component from 'vue-class-component';
 
 import './home.scss';
 
+interface Dependency {
+  package: string,
+  repo: string,
+}
+
+interface Author {
+  name: string,
+  origin: string,
+  position: string
+}
+
 @Component({
-    template: require('./home.html')
+  template: require('./home.html')
 })
 export class HomeComponent extends Vue {
 
-    package: string = 'vue-webpack-typescript';
-    repo: string = 'https://github.com/ducksoupdev/vue-webpack-typescript';
-    mode: string = process.env.ENV;
+  taitt : Author = {
+    name: 'To Tan Tai',
+    origin: 'k60.hus.edu',
+    position: 'Front-End Developer'
+  };
+
+  authors: Author[] = [
+    {
+      name: 'Thanh Pham Van',
+      origin: 'k60.hus.edu',
+      position: 'Full-Stack(OverFlow) Developer'
+    }
+    //, this.taitt
+  ];
+
+  main: Dependency = {
+    package: 'play-scala-vue-typescript-webpack',
+    repo: 'https://github.com/thanhphamvan/play-scala-vue-typescript-webpack'
+  };
+
+  dependencies: Dependency[] = [
+    {
+      package: 'vue-webpack-typescript',
+      repo: 'https://github.com/ducksoupdev/vue-webpack-typescript'
+    },
+    {
+      package: 'play-vue-webpack',
+      repo: 'https://github.com/gbogard/play-vue-webpack'
+    }
+  ];
+
+  mode: string = process.env.ENV;
 
 }
